2ad8292bf1d3db23365740bc2a591d31b1c408ee
NC4L4NGL4NG <calanglang.nea@obmontessori.edu.ph>
2024-12-10 05:58:14 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+<form action="file:///C:/Users/Neriza/Downloads/Q2%20Quiz%202/link%202.html#" method="GET">
+    <!-- Your form fields here -->
+    <input type="text" name="name" placeholder="Enter your name" required><br>
+    <input type="email" name="email" placeholder="Enter your email" required><br>
+    <button type="submit" class="submit-btn">Submit</button>
+</form>
